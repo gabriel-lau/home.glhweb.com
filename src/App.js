7585ad164e63254e30f9de5a9689b534ba676a79
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2022-09-21 22:42:09 +0800

    Update App.js
    Minor changes to App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className='App-body'>
       <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This site is a <code>WORK IN PROGRESS</code> 
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Made with React
         </a>
       </div>
       <Body1 />
