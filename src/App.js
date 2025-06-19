d2b87c94861ad0c4c69d716ecc96469fa78cbdb6
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2025-06-20 00:21:58 +0800

    more version upgrades

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Body1 from './body1/body1.js';
 import Body2 from './body2/body2.js';
 import Footer from './footer/footer.js';
-import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      {/*
+    return (
+        <div className="App">
+            {/*
       <div className='App-body'>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -23,11 +21,11 @@ function App() {
           Made with React
         </a>
       </div> */}
-      <Body1/>
-      <Body2/>
-      <Footer/>
-    </div>
-  );
+            <Body1 />
+            <Body2 />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
